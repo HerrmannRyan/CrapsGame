2652a7ecb045432ac7e19caae2fa09656f07f067
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2019-08-22 11:55:09 -0600

    Finished the Two Dice for 1-6

--- Craps Game/src/CrapsGame.java
+++ Craps Game/src/CrapsGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class CrapsGame
 	{
@@ -6,6 +7,20 @@ public class CrapsGame
 			{
 				// TODO Auto-generated method stub
 				//New Craps Game
+				Scanner userInput = new Scanner (System.in);
+				
+				// Welcoming player to the new game
+				System.out.println("Hello welcome to Craps. What is your name?");
+				
+				//Speaking Name
+				String userName = userInput.nextLine();
+				System.out.println("Hello " + userName + " Let\'s play a game!");
+				System.out.println("Press 1 to roll the dice!");
+				
+				//Rolling and creating the dice
+				
+				int diceOne = (int)(Math.random()*6 + 1);
+				int diceTwo = (int)(Math.random()*6 + 1);
 
 			}
 
