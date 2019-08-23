bf5338667c17db340642627bb3c6251d0459b1b4
rybla <rybla@Ryans-PC>
2019-08-23 16:31:23 -0600

    Committed from home

--- Craps Game/src/CrapsGame.java
+++ Craps Game/src/CrapsGame.java
@@ -5,7 +5,7 @@ public class CrapsGame
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				//Uploaded from Home 8-23-2019 @ 16:30
 				//New Craps Game
 				Scanner userInput = new Scanner (System.in);
 				
